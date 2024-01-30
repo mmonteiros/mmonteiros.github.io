2556365d69b7f3bac427e316b8420696e28ed31d
mmonteiros <mmonteirojs@gmail.com>
2024-01-30 20:36:37 -0300

    fix image header

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
     name: 'Mateus Monteiro',
     title: "Software Developer",
     description:"There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
-    image: 'https://raw.githubusercontent.com/PhantomScript/asset-container/main/developer-portfolio/landingImg.png',
+    image: 'https://raw.githubusercontent.com/mmonteiros/mmonteiros.github.io/ecf0e4fb2c8df7419c0cf940c52e3482d2ccd3dc/src/assets/profile_img.jpg',
     resumePdf: resume
 }
